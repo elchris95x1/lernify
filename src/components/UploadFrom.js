bf32a3f618970fd6f5981c49ec3cb91d725918d5
Chris <achris95x1@gmail.com>
2024-05-13 01:13:51 -0500

    Delete .env

--- src/components/UploadFrom.js
+++ src/components/UploadFrom.js
@@ -1,23 +1,43 @@
 'use client'
 import UploadIcon from "@/components/UploadIcon";
+import { useState } from "react";
+import axios from "axios";
+import {useRouter} from "next/navigation";
 
-export default function UploadForm(){
-    async function upload(ev){
-        ev.preventDefult();
-        const files = ev.target.files;
-        if(files.length > 0){
-          const file = files[0];
-          const res = await axios.postForm('/api/upload', {
-            file,
-          });
-          console.log(res.data);
-        }
-      }
-    return(
-        <label className="bg-green-400 py-2 px-4 rounded-full inline-flex gap-2 border-2 border-purple-700 cursor-pointer">
-          <UploadIcon/>
-          <span>Choose file</span>
-          <input onChange={upload} type="file" className="hidden"/>
-        </label>
+export default function UploadFrom(){
+  const [isUploading, setIsUploading] = useState(false);
+  const router = useRouter();
+
+  async function upload(ev) {
+    ev.preventDefault();
+    const files = ev.target.files;
+    if (files.length > 0) {
+      const file = files[0];
+      setIsUploading(true);
+      const res = await axios.postForm('/api/upload', {
+        file,
+      });
+      setIsUploading(false);
+      const newName = res.data.newName;
+      router.push('/'+newName);
+    }
+  }
+
+  return (
+    <>
+      {isUploading && (
+        <div className="bg-black/90 text-white fixed inset-0 flex items-center">
+          <div className="w-full text-center">
+            <h2 className="text-4xl mb-4">Uploading</h2>
+            <h3 className="text-xl">Please wait...</h3>
+          </div>
+        </div>
+      )}
+      <label className="bg-green-600 py-2 px-6 rounded-full inline-flex gap-2 border-2 border-purple-700/50 cursor-pointer">
+        <UploadIcon />
+        <span>Choose file</span>
+        <input onChange={upload} type="file" className="hidden"/>
+      </label>
+    </>
     )
 }
